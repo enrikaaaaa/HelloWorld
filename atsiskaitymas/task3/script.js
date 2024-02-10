5a704564e7947e19f0627367a78a37a6db767a6c
enrikaaaaa <122116349+enrikaaaaa@users.noreply.github.com>
2024-02-10 20:27:41 +0200

    Update script.js

--- atsiskaitymas/task3/script.js
+++ atsiskaitymas/task3/script.js
@@ -8,22 +8,22 @@ division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 function Calculator(a,b) {
     this.a = a;
     this.b = b;
-    this.sum = function () { //Funkcija sumos skaiciavimui
+    this.sum = function () { 
       return a + b;
     };
-    this.subtraction = function () { //Funkcija skirtumo skaiciavimui
+    this.subtraction = function () { 
       return a - b;  
     };
-    this.multiplication = function () { //Funkcija daugybos skaiciavimui
+    this.multiplication = function () { 
         return a * b;
    };
-    this.division = function () { //Funkcija dalybos skaiciavimui
+    this.division = function () {
          return a/b;
     };
   }
   
-  const create = new Calculator(10, 20); //Sukuriam naują objekta, jei reikia nors ir 20
-  console.log(`Suma yra ${create.sum()}`);  //Isvedam i console sumą
-  console.log(`Skirtumas yra ${create.subtraction()}`);   //Isvedam i console skirtumą
-  console.log(`Daugyba : ${create.multiplication()}`);  //Isvedam i console daugybą
-  console.log(`Dalyba : ${create.division().toFixed(2)}`);  //Isvedam i console dalybą su 2 skaičiais po kablelio
+  const create = new Calculator(10, 20); 
+  console.log(`Suma yra ${create.sum()}`);  
+  console.log(`Skirtumas yra ${create.subtraction()}`);
+  console.log(`Daugyba : ${create.multiplication()}`);
+  console.log(`Dalyba : ${create.division().toFixed(2)}`);  
